9f9a87efd95fb891781a0c8b76c62331d9cdc2f0
Sadia-Tasnim-Meem <tasnimmeem674@gmail.com>
2023-06-22 02:16:51 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*========== scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+};
+
+
